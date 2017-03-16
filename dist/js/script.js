68fd1f11d1472187ab9dc26112951cecf73e0e9c
deadly8 <tarasent@ukr.net>
2017-03-16 19:23:03 +0200

    created major sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,19 @@
-$("#example_id").ionRangeSlider({
-    type: "double",
-    grid: true,
-    min: 0,
-    max: 11
+$(document).ready(function() {
+    $('#imageGallery').lightSlider({
+        gallery:true,
+        item:1,
+        loop: true,
+        enableDrag: true,
+        slideMove: 2,
+        thumbItem:4,
+        slideMargin:0,
+
+        currentPagerPosition:'middle',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#imageGallery .lslide'
+            });
+        }
+    });
 });
-$("#price").ionRangeSlider({
-    type: "double",
-    grid: true,
-    min: 0,
-    max: 1000,
-    prefix: "$"
-});
+
