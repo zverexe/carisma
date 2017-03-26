2116eba9ea57f255aa4674d1637ed7aeb1dcca63
deadly8 <tarasent@ukr.net>
2017-03-26 22:12:51 +0300

    added user-profile page

--- js/script.js
+++ js/script.js
@@ -1,34 +1,32 @@
-$(document).ready(function() {
-    $('#imageGallery').lightSlider({
-        gallery:true,
-        item:1,
-        loop: true,
-        enableDrag: true,
-        slideMove: 2,
-        thumbItem:4,
-        slideMargin:0,
-        currentPagerPosition:'middle',
-        onSliderLoad: function(el) {
-            el.lightGallery({
-                selector: '#imageGallery .lslide'
-            });
-        }
-    });
-    $("#example_id").ionRangeSlider({
-        type: "double",
-        grid: true,
-        min: 0,
-        max: 11
-    });
-    $("#price").ionRangeSlider({
-        type: "double",
-        grid: true,
-        min: 0,
-        max: 1000,
-        prefix: "$"
-    });
 
-
+$('#imageGallery').lightSlider({
+    gallery:true,
+    item:1,
+    loop: true,
+    enableDrag: true,
+    slideMove: 2,
+    thumbItem:4,
+    slideMargin:0,
+    currentPagerPosition:'middle',
+    onSliderLoad: function(el) {
+        el.lightGallery({
+            selector: '#imageGallery .lslide'
+        });
+    }
 });
 
 $('[data-toggle="tooltip"]').tooltip();
+
+$("#example_id").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 11
+});
+$("#price").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 1000,
+    prefix: "$"
+});
